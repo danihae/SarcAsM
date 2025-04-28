4163134425a0e5c7efe4aac0dfb997c5a3a0bb4f
danihae <dani.hae@posteo.de>
2025-04-28 14:48:51 +0200

    Rename and update CUDA version in virtual environment creation script

--- dist/create_clean_venv_cuda118.cmd
+++ dist/create_clean_venv_cuda118.cmd
@@ -2,10 +2,9 @@
 rem "need a python distribution in path to actually build the venv"
 python -m venv --copies venv
 venv\Scripts\python.exe -m pip install --upgrade pip
-venv\Scripts\python.exe -m pip install git+https://github.com/danihae/bio-image-unet
 venv\Scripts\python.exe -m pip install -r requirements.txt
 venv\Scripts\python.exe -m pip uninstall -y torch
 rem "todo: it would be better (maybe) to uninstall torch before the next command"
-venv\Scripts\python.exe -m pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu117
+venv\Scripts\python.exe -m pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
 rem "torch+cuda after requirements, otherwise it will get overridden"
 pause
