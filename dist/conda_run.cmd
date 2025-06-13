cb92450abe56fced4f2c8785c67d245851af99fc
daniel <dani.hae@posteo.de>
2025-06-13 21:44:22 +0200

    Remove obsolete distribution scripts and related files for minimal distribution, including setup guides, batch files, and README. Clean up unused resources.

--- dist/conda_run.cmd
+++
@@ -1,4 +0,0 @@
-conda activate sarcasm_venv
-echo Calling Python __main__.py, this could take a while to start the GUI for SARCASM.
-python __main__.py
-conda deactivate sarcasm_venv
--- dist/create_clean_venv.cmd
+++
@@ -1,6 +0,0 @@
-@echo off
-rem "need a python distribution in path to actually build the venv"
-python -m venv --copies venv
-venv\Scripts\python.exe -m pip install --upgrade pip
-venv\Scripts\python.exe -m pip install -r requirements.txt
-pause
